70524eb46194833b3877d87e06dd3023a3276f54
Nam Le <nam.leduc02@gmail.com>
2021-01-15 10:13:30 +0100

    Add label

--- src/MainPanel.tsx
+++ src/MainPanel.tsx
@@ -53,12 +53,16 @@ export class MainPanel extends PureComponent<Props, State> {
         style={{
           width,
           height,
-          padding: 10,
           display: 'flex',
           justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
         <CircularProgressbar value={num >= threshold ? threshold : (num / threshold) * 100} text={num.toString()} />
+        <div style={{ marginTop: 15, fontFamily: 'Brush Script MT, cursive', fontSize: '4em', textAlign: 'center' }}>
+          Πελάτες εντός καταστήματος
+        </div>
       </div>
     );
   }
